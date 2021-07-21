933a576dddedadad97662703b36a63dd65e3b2ca
Filipe Drummond <exgavian82@gmail.com>
2021-07-21 02:59:26 -0300

    :sparkles: Configure project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 const App: React.FC = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>App</Text>
-    </View>
+    <NavigationContainer>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text style={{ fontFamily: 'Inter-Regular', fontSize: 20 }}>App</Text>
+      </View>
+    </NavigationContainer>
   );
 };
 
